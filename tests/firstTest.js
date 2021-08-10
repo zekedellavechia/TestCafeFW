855157a265319f8e099ed513e8bfbab55a426b2c
Ezequiel D <ezequiel.dellavechia@gmail.com>
2021-08-10 01:55:10 -0300

    Added locators to the tests

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,40 +1,41 @@
 import { Selector } from 'testcafe';
 
+const firstName = Selector('#first-name')
+const lastName = Selector('#last-name')
+const jobTitle = Selector('#job-title')
+const radioButton1 = Selector('#radio-button-1')
+const radioButton2 = Selector('#radio-button-2')
+const radioButton3 = Selector('#radio-button-3')
+const checkbox1 = Selector('#checkbox-1')
+const checkbox2 = Selector('#checkbox-2')
+const selectMenu = Selector('#select-menu')
+const option = Selector('option')
+const dateSelect = Selector('#datepicker')
+const formGroup = Selector('.form-group')
+const buttonSubmit = Selector('a').withText('Submit')
+const successText = Selector('h1')
+
 fixture `Visit Test site`
     .page `https://formy-project.herokuapp.com/form`;
 
 test('Fill form one', async t => {
     await t
-        .typeText(Selector('#first-name'), 'Zeke')
-        .typeText(Selector('#last-name'), 'Dellavechia')
-        .typeText(Selector('#job-title'), 'QA Automation')
-        .click(Selector('#radio-button-2'))
-        .click(Selector('#checkbox-1'))
-        .click(Selector('#select-menu'))
-        .click(Selector('option').withText('10+'))
-        .typeText(Selector('#datepicker'), '10/10/1988')
-        .click(Selector('.form-group'))
-        .click(Selector('a').withText('Submit'))
-    });
+        .typeText(firstName, 'Zeke')
+        .typeText(lastName, 'Dellavechia')
+        .typeText(jobTitle, 'QA Automation')
+        .click(radioButton2)
+        .click(checkbox1)
+        .click(selectMenu)
+        .click(option.withText('10+'))
+        .typeText(dateSelect, '10/10/1988')
+        .click(formGroup)
+        .click(buttonSubmit)
+});
 
-    test('Fill form two', async t => {
-        await t
-            .typeText(Selector('#first-name'), 'Licha')
-            .typeText(Selector('#last-name'), 'Navarrete')
-            .typeText(Selector('#job-title'), 'QA Engineer')
-            .click(Selector('#radio-button-3'))
-            .click(Selector('#checkbox-1'))
-            .click(Selector('#select-menu'))
-            .click(Selector('option').withText('5-9'))
-            .typeText(Selector('#datepicker'), '11/11/1994')
-            .click(Selector('.form-group'))
-            .click(Selector('a').withText('Submit'))
-        });
 
-        test('Verify form can be submitted', async t => {
-            const successText = Selector('h1')
 
-            await t
-                .click(Selector('a').withText('Submit'))
-                .expect(successText.textContent).eql('Thanks for submitting your form');
-        });    
+test('Verify form can be submitted', async t => {
+    await t
+        .click(buttonSubmit)
+        .expect(successText.textContent).eql('Thanks for submitting your form');
+});    
